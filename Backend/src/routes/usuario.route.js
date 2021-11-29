08d272080138bbad8db1279cfef9c70652afe091
Jorgelasso1992 <jorgelasso_1992@hotmail.com>
2021-11-29 15:02:36 -0500

    Aceptando cambios

--- Backend/src/routes/usuario.route.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require('express');
-const router = Router();
-const usuarioController=require('../controller/usuario.controller');
-
-router.post('/crearusuario', usuarioController.crearUsuario);
-router.post('/login', usuarioController.login);
-
-module.exports=router;
